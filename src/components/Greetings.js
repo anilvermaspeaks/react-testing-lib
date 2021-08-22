c8b3e37e3367486d6846f1da9a28d6e61872ba6e
anil verma <anilcs240@gmail.com>
2021-08-22 17:47:06 +0530

    test stat based comp

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,9 +1,21 @@
+import { buildQueries } from "@testing-library/dom";
+import { useState } from "react";
+
 const Greeting = () => {
+
+    const [updatedText, updateText] = useState(false);
+
+    const contentChangeHandler = () => {
+        updateText(true)
+    }
+
+
     return (
         <div>
             <h1>Hello World</h1>
-            <p>Good to see you!!!</p>
-        </div>
+            {!updatedText && <p>Good to see you!!!</p>}
+            <button role="tab" onClick={contentChangeHandler}>Update Text</button>
+        </div >
     )
 }
 
--- src/components/Greetings.test.js
+++ src/components/Greetings.test.js
@@ -1,11 +1,33 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greetings'
 
-test('renders hello worls as tetx', () => {
-    render(<Greeting />);
-    const hellNode = screen.getByText(/hello/i);
 
-    expect(hellNode).toBeInTheDocument();
+describe('Greeting component', () => {
 
+    test('renders hello worls as tetx', () => {
+        render(<Greeting />);
+        const hellNode = screen.getByText(/hello/i);
+
+        expect(hellNode).toBeInTheDocument();
+
+
+    })
+
+    test('render good to see you if button is not clicked', () => {
+        render(<Greeting />);
+        const paraNode = screen.getByText(/Good to see you/i);
+        expect(paraNode).toBeInTheDocument()
+    });
+
+    test('not render good to see you if button is clicked', () => {
+        render(<Greeting />);
+        const btnEle = screen.getByRole(
+            'tab'
+        )
+        userEvent.click(btnEle)
+        const paraode = screen.queryByText(/Good to see you/i);
+        expect(paraode).not.toBeInTheDocument();
+    });
 
 })
